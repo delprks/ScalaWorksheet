142c51fef0d5b5ee6c80a033190b3dc269ddd8b4
Daniel Parks <daniel.parks@bbc.co.uk>
2016-02-10 13:45:34 +0000

    working on rational

--- src/Rational.scala
+++ src/Rational.scala
@@ -3,16 +3,30 @@ class Rational(n: Int, d: Int) {
   val numer:Int = n
   val denom:Int = d
   override def toString = n + "/" + d
-  def add(that: Rational): Rational = new Rational(
+
+  def this(n: Int) = this(n, 1) // auxiliary constructor
+
+  def +(that: Rational): Rational = new Rational(
     numer * that.denom + that.numer * denom, denom * that.denom
   )
+
+  def + (i: Int): Rational = new Rational(numer + i * denom, denom)
+
+  def < (that: Rational): Boolean = this.numer * that.denom < that.numer * this.denom
+
+  def max(that: Rational) = if (this < that) that else this
 }
 
 object Rational {
   def main(args: Array[String]): Unit = {
     val oneHalf = new Rational(1, 2)
     val twoThirds = new Rational(2, 3)
+    val five = new Rational(5)
 
-    println(oneHalf add twoThirds)
+    println(oneHalf + twoThirds)
+    println(oneHalf < twoThirds)
+    println(oneHalf max twoThirds)
+    println(five)
+    println(oneHalf + 4)
   }
 }
