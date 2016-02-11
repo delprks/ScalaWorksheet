342e86a9eb5b25754be2994212b05c3a12fabb04
Daniel Parks <daniel.parks@bbc.co.uk>
2016-02-11 13:38:34 +0000

    working on control structures

---
+++ src/ControlStructures.scala
@@ -0,0 +1,18 @@
+import java.io.File
+
+val test = if (args.nonEmpty) args(0) else "default"
+
+println(test)
+
+def fileLines(file: java.io.File) =
+  scala.io.Source.fromFile(file).getLines().toList
+
+val filesHere = new File(".").listFiles()
+
+for {
+  file <- filesHere
+  if file.getName.endsWith(".scala")
+  line <- fileLines(file)
+  trimmed = line.trim
+  if trimmed.matches(".*filesHere.*")
+} println(file + ": " + line.trim)
