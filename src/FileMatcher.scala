221bc52be9539ea9d8beae99ce74e98d8b11ed50
Daniel Parks <daniel.parks@bbc.co.uk>
2016-02-18 16:22:16 +0000

    using placeholders in methods

--- src/FileMatcher.scala
+++ src/FileMatcher.scala
@@ -4,12 +4,15 @@ object FileMatcher {
   private def filesHere = new File(".").listFiles()
 
   def filesEnding(query: String) =
-    for (file <- filesHere; if file.getName.endsWith(query))
-      yield file
+    fileMatching(query, _.endsWith(_))
 
   def fileContains(query: String) =
-    for (file <- filesHere; if file.getName.contains(query))
+    fileMatching(query, _.contains(_))
+
+  def fileMatching(query: String, matcher: (String, String) => Boolean) = {
+    for (file <- filesHere; if matcher(file.getName, query))
       yield file
+  }
 }
 
 val filesEndWith = FileMatcher.filesEnding(".scala")
