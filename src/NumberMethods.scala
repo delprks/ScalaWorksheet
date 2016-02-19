ce308120f96d03cfeaa72381f02ab09eb191e9fd
Daniel Parks <daniel.parks@bbc.co.uk>
2016-02-19 12:29:41 +0000

    added contains odd method

--- src/NumberMethods.scala
+++ src/NumberMethods.scala
@@ -1,3 +1,11 @@
 def containsNeg(nums: List[Int]) = nums.exists(_ < 0)
+def containsOdd(nums: List[Int]): Boolean = {
+  var exists = false
+  for (num <- nums)
+    if (num % 2 == 1)
+      exists = true
+  exists
+}
 
 println(containsNeg(List(10, 12, 4, 2, -2)))
+println(containsOdd(List(19, 10, 2)));
