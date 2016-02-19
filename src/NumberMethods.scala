f532e443e401c31ff37b064bddd767c062043ae3
Daniel Parks <daniel.parks@bbc.co.uk>
2016-02-19 12:33:22 +0000

    more functional

--- src/NumberMethods.scala
+++ src/NumberMethods.scala
@@ -1,11 +1,5 @@
 def containsNeg(nums: List[Int]) = nums.exists(_ < 0)
-def containsOdd(nums: List[Int]): Boolean = {
-  var exists = false
-  for (num <- nums)
-    if (num % 2 == 1)
-      exists = true
-  exists
-}
+def containsOdd(nums: List[Int]) = nums.exists(_ % 2 == 1)
 
 println(containsNeg(List(10, 12, 4, 2, -2)))
 println(containsOdd(List(19, 10, 2)));
