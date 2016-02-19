8bdf87f448a22ed7b50aaa146d179f30658c9888
Daniel Parks <daniel.parks@bbc.co.uk>
2016-02-19 13:08:33 +0000

    currying

--- src/Currying.scala
+++ src/Currying.scala
@@ -1,7 +1,8 @@
 def normalSum(num1: Int, num2: Int) = num1 + num2
 def curriedSum(x: Int)(y: Int) = x + y
 val onePlus = curriedSum(1)_
+val normalOnePlus = normalSum(1, _: Int)
 
 println(normalSum(2, 3))
 println(curriedSum(1)(2))
-println(onePlus(3))
+println(normalOnePlus(4))
