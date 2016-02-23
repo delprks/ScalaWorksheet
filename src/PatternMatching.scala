4edb85385bb5768d108ec4a7d46a538be37a02ed
Daniel Parks <daniel.parks@bbc.co.uk>
2016-02-23 15:24:52 +0000

    pattern matching

--- src/PatternMatching.scala
+++ src/PatternMatching.scala
@@ -1,11 +1,14 @@
 def listAnalysis(list: List[Any]) = list match {
   case Nil => "Empty"
-  case "a" :: tail => "starts with 'a'"
+  case "a" :: tail => "starts with 'a'" //tail is just a name, could've written as case 'a' :: _ => "starting by 'a'"
   case (head:Int) :: _ if head > 3 => "starting by an int greater than 3"
   case (head:Int) :: _ => "starting by an int"
   case _ => "Whatever"
 }
 
-val list = List(5, "b", "a")
-
-println(listAnalysis(list))
+println(listAnalysis(List()))                             //> java.lang.String = empty
+println(listAnalysis("This is a test".toList))            //> java.lang.String = whatever
+println(listAnalysis("abcde".toList))                     //> java.lang.String = starting by 'a'
+println(listAnalysis(List(1,2,3)))                        //> java.lang.String = starting by an int
+println(listAnalysis(List(42,24,36)))                     //> java.lang.String = starting by an int greater than 3
+println(listAnalysis("a".toList))                         //> java.lang.String = starting by 'a'
