9bc090375bd8fceb9bea87e4375144259b703c43
Daniel Parks <daniel.parks@bbc.co.uk>
2016-02-25 13:52:52 +0000

    experimenting with sets and maps

---
+++ src/CountWords.scala
@@ -0,0 +1,18 @@
+import scala.collection.mutable
+
+def countWords(text: String) = {
+  val counts = mutable.Map.empty[String, Int]
+
+  for (rawWord <- text.split("[ ,!. ]+")) {
+    val word = rawWord.toLowerCase
+    val oldCount =
+      if (counts.contains(word)) counts(word)
+      else 0
+
+    counts += (word -> (oldCount + 1))
+  }
+
+  counts
+}
+
+println(countWords("See Spot run! Run, Spot. Run!"))
