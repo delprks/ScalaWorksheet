e051489cceafdf7530e268b78d5fe01d7f308fc6
Daniel Parks <daniel.parks@bbc.co.uk>
2016-02-25 15:13:49 +0000

    experimenting with tuples

---
+++ src/LongestWord.scala
@@ -0,0 +1,18 @@
+
+def longestWord(words: Array[String]) = {
+  var word = words(0)
+  var idx = 0
+
+  for (i <- 1 until words.length) {
+    if (words(i).length > word.length) {
+      word = words(i)
+      idx = i
+    }
+  }
+
+  (word, idx)
+}
+
+val (word, index)  = longestWord("The quick brown fox".split(" "))
+
+println(s"longest word is $word with index of $index")
