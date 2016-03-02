50f57bd088f747847a6fc5f7b6e61aeac2df0bfc
Daniel Parks <daniel.parks@bbc.co.uk>
2016-03-02 15:23:41 +0000

    ordering people

---
+++ src/Person.scala
@@ -0,0 +1,17 @@
+class Person (val firstName: String, val lastName: String) extends Ordered[Person] {
+  def compare(that: Person) = {
+    val lastNameComparison = lastName.compareToIgnoreCase(that.lastName)
+
+    if (lastNameComparison != 0)
+      lastNameComparison
+    else
+      firstName.compareToIgnoreCase(that.firstName)
+  }
+
+  override def toString = firstName + " " + lastName
+}
+
+val robert = new Person("Robert", "Jones")
+val sally = new Person("Sally", "Smith")
+
+println(robert < sally)
